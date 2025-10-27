1c87856e9b6a100b7710d8d06ef367426874b090
sivakumar reddy <info@joindevops.com>
2025-10-27 13:02:19 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -36,3 +36,42 @@ resource "terraform_data" "mongodb" {
     ]
   }
 }
+
+resource "aws_instance" "redis" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.mongodb_sg_id]
+  subnet_id = local.database_subnet_id
+
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${var.project_name}-${var.environment}-mongoredisdb"
+    }
+  )
+}
+
+resource "terraform_data" "redis" {
+  triggers_replace = [
+    aws_instance.redis.id
+  ]
+  
+  provisioner "file" {
+    source      = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.redis.private_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh redis ${var.environment}"
+    ]
+  }
+}
