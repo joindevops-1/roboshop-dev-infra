da0d9da64c27f68ee8903ba71a8a1620604c5b09
sivakumar reddy <info@joindevops.com>
2025-10-29 06:50:36 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -9,26 +9,27 @@
 
 REPO_URL="https://github.com/daws-86s/ansible-roboshop-roles-tf.git"
 REPO_DIR="/opt/roboshop/ansible"
+ANSIBLE_DIR=ansible-roboshop-roles-tf
 
 component="$1"
 
 dnf install -y ansible git
 mkdir -p $REPO_DIR
 
+cd $REPO_DIR/
 # Ensure repo directory exists
-if [[ ! -d "$REPO_DIR" ]]; then
+if [[ ! -d "$ANSIBLE_DIR" ]]; then
   echo "🔄 Cloning repository..."
-  git clone "$REPO_URL" "$REPO_DIR"
+  git clone "$REPO_URL"
+  cd "$ANSIBLE_DIR"
 else
   echo "📥 Repository exists, pulling latest changes..."
-  cd "$REPO_DIR"
+  cd "$ANSIBLE_DIR"
   git reset --hard
   git pull
 fi
 
 mkdir -p /var/log/roboshop
 touch /var/log/roboshop/ansible.log
-
-cd $REPO_DIR/ansible-roboshop-roles-tf
 
 ansible-playbook -e component=$component main.yaml
